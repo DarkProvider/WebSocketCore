b828798d3f406c23b3531c6dfeab6bf2cd051e9b
Meap <meapsw@gmail.com>
2021-06-02 19:44:25 +0200

    Fixed a typo

--- Client/Main/Settings.cs
+++ Client/Main/Settings.cs
@@ -8,7 +8,7 @@ namespace Client.Main
         // To set up SSL please check the official websocketsharp documentation on sta's GitHub page.
 
         // Host setup.
-        internal static readonly string IP = "127.00.0.1";
+        internal static readonly string IP = "127.0.0.1";
         internal static readonly int port = 8880;
         internal static readonly string servicename = "/chat"; // Example: /chat (so it would be yourdomain.com/chat)
         internal static readonly string hostname = $"ws://{IP}:{port}{servicename}";
